f6068252142974b195734bcc76ca73b5b73856fa
fernandodefez <fernandodefez@outlook.com>
2022-08-31 14:49:44 -0500

    add remote database credentials

--- includes/config/database.php
+++ includes/config/database.php
@@ -1,11 +1,11 @@
 <?php 
 
-function conectarDB() : mysqli {
-    $db = mysqli_connect('localhost', 'root', 'root', 'galeria');
+function conectarDB() {
 
-    if(!$db){
+    $db = mysqli_connect('creativolab.com.mx', 'creativolabcom_pics', '532IR842B4', 'creativolabcom_pics');
+
+    if(!$db) {
         echo 'Hubo un error al conectar la base de datos';
     }
-
     return $db;
 }
